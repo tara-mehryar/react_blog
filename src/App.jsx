3eb12f08465cf5e3f29ea06973b8360f97510505
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-30 09:33:02 -0500

    added BrowserRouter and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import Nav from './components/Nav/Nav.jsx';
+import { BrowserRouter, Routes } from 'react-router-dom';
+
 
 export default function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Nav />
       <h1 style={{ padding: '200px 35%' }}>This is where your pages will appear</h1>
     </div>
+    </BrowserRouter>
   );
 }
